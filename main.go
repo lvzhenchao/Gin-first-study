3e773397e6cf196374ed51a96224f17f8a9126d4
lvzhenchao <1370164516@qq.com>
2021-08-14 21:26:32 +0800

    创建engine实例的两种方式

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 )
 
 func main()  {
+	//创建Engine
 	engine := gin.Default()
 
 	//处理get请求
@@ -23,3 +24,22 @@ func main()  {
 
 //可以在命令行使用： curl http://localhost:8080/hello
 //engine.Run()可以自定义端口号，默认8080
+
+//engine代表gin框架的一个结构体定义，包含路由组，中间件、页面渲染接口、框架配置等
+//两种方式
+//gin.Default()
+//gin.New()
+//区别
+//gin.Default也使用gin.New创建实例，但是会默认使用Logger和Recovery中间件
+//logger负责打印输出日志的中间件，方便开发者进行程序调试
+//recovery中间件作用是程序执行过程中遇到panic中断了服务，则recovery会恢复程序执行，并返回服务器500内部错误
+//通常我们使用Default创建engine实例
+
+
+
+
+
+
+
+
+
